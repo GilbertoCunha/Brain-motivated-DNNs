02efc5680f085303e35a6351e22fd69478a191b4
Gilberto Cunha <gcacademic@outlook.pt>
2020-08-22 15:39:05 +0100

    restructure repo

---
+++ SIFT/SIFT_Retina_VVS.py
@@ -0,0 +1,97 @@
+from torch.utils.data import DataLoader, random_split
+from pytorch_lightning import loggers as pl_loggers
+from SIFT.SIFT_classes import SIFTRetinaVVS
+from torchvision.datasets import CIFAR10
+from argparse import ArgumentParser
+from torchvision import transforms
+import pytorch_lightning as pl
+import pandas as pd
+import optuna
+import torch
+
+
+def objective(trial, args, search):
+    # Optuna trial parameters
+    batch_size = trial.suggest_categorical("batch_size", search["batch_size"])
+    params = {
+        "ret_channels": trial.suggest_categorical("ret_channels", search["ret_channels"]),
+        "vvs_layers": trial.suggest_categorical("vvs_layers", search["vvs_layers"]),
+        "dropout": trial.suggest_categorical("dropout", search["dropout"]),
+        "sift_type": trial.suggest_categorical("sift_type", search["sift_type"]),
+        "lr": 1e-3
+    }
+
+    # Train and validation dataloaders
+    transform = transforms.Compose([
+        transforms.Grayscale(1),
+        transforms.ToTensor()
+    ])
+    train_data = CIFAR10(root="data/CIFAR10", download=False, train=True, transform=transform)
+    input_shape = tuple(train_data[0][0].shape)
+    val_size = int(0.2 * len(train_data))
+    train_size = len(train_data) - val_size
+    train_data, val_data = random_split(train_data, [train_size, val_size],
+                                        generator=torch.Generator().manual_seed(trial.number))
+    train_data = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=12)
+    val_data = DataLoader(val_data, batch_size=batch_size, num_workers=12)
+
+    # Create model
+    params["input_shape"] = input_shape
+    if params["sift_type"] != "none":
+        patch_size = trial.suggest_categorical("patch_size", search["patch_size"])
+        params["patch_size"] = patch_size
+        file_name = f"SIFT_{params['sift_type']}/"
+    else:
+        file_name = "RetinaVVS/"
+    model = SIFTRetinaVVS(params)
+
+    # Callbacks
+    early_stop = pl.callbacks.EarlyStopping(
+        monitor="val_loss",
+        patience=args.es_patience,
+        mode="min"
+    )
+    model_checkpoint = pl.callbacks.ModelCheckpoint(
+        filepath=f"SIFT/models/{file_name}",
+        prefix=model.name,
+        monitor="val_acc",
+        mode="max",
+        save_top_k=1
+    )
+    tb_logger = pl_loggers.TensorBoardLogger(f"SIFT/logs/{file_name}", name=model.name)
+
+    # Train the model
+    trainer = pl.Trainer.from_argparse_args(args, early_stop_callback=early_stop, num_sanity_val_steps=0,
+                                            checkpoint_callback=model_checkpoint, auto_lr_find=True,
+                                            logger=tb_logger, fast_dev_run=False, max_epochs=100)
+    trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
+
+    return model_checkpoint.best_model_score
+
+
+if __name__ == "__main__":
+    # Terminal Arguments
+    parser = ArgumentParser()
+    parser.add_argument("--n_trials", type=int, default=1)
+    parser.add_argument("--es_patience", type=int, default=3)
+    parser.add_argument("--gpus", type=int, default=1)
+    parser.add_argument("--sift_type", type=str, default="none")
+    parser_args = parser.parse_args()
+
+    # Optuna Hyperparameter Study
+    study_name = "test"
+    search_space = {
+        'batch_size': [128],
+        'ret_channels': [4],
+        'vvs_layers': [5],
+        'dropout': [0.0],
+        'sift_type': [parser_args.sift_type]
+    }
+    study = optuna.create_study(direction="maximize", sampler=optuna.samplers.GridSampler(search_space))
+    study.optimize(lambda trials: objective(trials, parser_args, search_space), n_trials=parser_args.n_trials)
+
+    # Process study dataframe
+    study_df = study.trials_dataframe()
+    study_df.rename(columns={"value": "val_acc", "number": "trial"}, inplace=True)
+    study_df.drop(["datetime_start", "datetime_complete"], axis=1, inplace=True)
+    study_df.to_hdf(f"SIFT/studies/{study_name}.h5", key="study")
--- SIFT/SIFT_classes.py
+++ SIFT/SIFT_classes.py
@@ -1,16 +1,9 @@
-from sklearn.metrics import roc_auc_score
-from torch.utils.data import DataLoader, random_split
-from pytorch_lightning import loggers as pl_loggers
 from kornia.feature.siftdesc import SIFTDescriptor
-from torchvision.datasets import CIFAR10
-from argparse import ArgumentParser
-from torchvision import transforms
+from sklearn.metrics import roc_auc_score
 import torch.nn.functional as F
 import pytorch_lightning as pl
 import torch.nn as nn
-import pandas as pd
 import numpy as np
-import optuna
 import torch
 import time
 
@@ -209,92 +202,3 @@ class SIFTRetinaVVS(pl.LightningModule):
         }
 
         return results
-
-
-def objective(trial, args, search):
-    # Optuna trial parameters
-    batch_size = trial.suggest_categorical("batch_size", search["batch_size"])
-    ret_channels = trial.suggest_categorical("ret_channels", search["ret_channels"])
-    vvs_layers = trial.suggest_categorical("vvs_layers", search["vvs_layers"])
-    sift_type = trial.suggest_categorical("sift_type", search["sift_type"])
-    dropout = trial.suggest_categorical("dropout", search["dropout"])
-
-    # Train and validation dataloaders
-    transform = transforms.Compose([
-        transforms.Grayscale(1),
-        transforms.ToTensor()
-    ])
-    train_data = CIFAR10(root="data/CIFAR10", download=False, train=True, transform=transform)
-    input_shape = tuple(train_data[0][0].shape)
-    val_size = int(0.2 * len(train_data))
-    train_size = len(train_data) - val_size
-    train_data, val_data = random_split(train_data, [train_size, val_size],
-                                        generator=torch.Generator().manual_seed(trial.number))
-    train_data = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=12)
-    val_data = DataLoader(val_data, batch_size=batch_size, num_workers=12)
-
-    # Create model
-    params = {
-        "ret_channels": ret_channels,
-        "vvs_layers": vvs_layers,
-        "dropout": dropout,
-        "sift_type": sift_type,
-        "input_shape": input_shape,
-        "lr": 1e-3
-    }
-    if params["sift_type"] != "none":
-        patch_size = trial.suggest_categorical("patch_size", search["patch_size"])
-        params["patch_size"] = patch_size
-        file_name = f"SIFT_{sift_type}/"
-    else:
-        file_name = "RetinaVVS/"
-    model = SIFTRetinaVVS(params)
-
-    # Callbacks
-    early_stop = pl.callbacks.EarlyStopping(
-        monitor="val_loss",
-        patience=args.es_patience,
-        mode="min"
-    )
-    model_checkpoint = pl.callbacks.ModelCheckpoint(
-        filepath=f"models/{file_name}",
-        prefix=model.name,
-        monitor="val_acc",
-        mode="max",
-        save_top_k=1
-    )
-    tb_logger = pl_loggers.TensorBoardLogger(f"logs/{file_name}", name=model.name)
-
-    # Train the model
-    trainer = pl.Trainer.from_argparse_args(args, early_stop_callback=early_stop, num_sanity_val_steps=0,
-                                            checkpoint_callback=model_checkpoint, auto_lr_find=True,
-                                            logger=tb_logger, fast_dev_run=False, max_epochs=100)
-    trainer.fit(model, train_dataloader=train_data, val_dataloaders=val_data)
-
-    return model_checkpoint.best_model_score
-
-
-if __name__ == "__main__":
-    # Terminal Arguments
-    parser = ArgumentParser()
-    parser.add_argument("--n_trials", type=int, default=20)
-    parser.add_argument("--es_patience", type=int, default=3)
-    parser.add_argument("--gpus", type=int, default=1)
-    parser_args = parser.parse_args()
-
-    # Optuna Hyperparameter Study
-    search_space = {
-        'batch_size': [32],
-        'ret_channels': [64],
-        'vvs_layers': [5],
-        'dropout': [0.0],
-        'sift_type': ["none"]
-    }
-    study = optuna.create_study(direction="maximize", sampler=optuna.samplers.GridSampler(search_space))
-    study.optimize(lambda trials: objective(trials, parser_args, search_space), n_trials=parser_args.n_trials)
-
-    # Process study dataframe
-    study_df = study.trials_dataframe()
-    study_df.rename(columns={"value": "val_acc", "number": "trial"}, inplace=True)
-    study_df.drop(["datetime_start", "datetime_complete"], axis=1, inplace=True)
-    study_df.to_hdf(f"studies/retina_vvs_grid5.h5", key="study")
